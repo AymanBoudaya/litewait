3730cfcce588357dada42a8f3b1dfaf64c095cb8
Ayman BOUDAYA <ayman.boudaya@yahoo.fr>
2025-05-06 07:28:18 +0100

    Serach bar 1

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,8 @@
+import 'package:caferesto/utils/device/device_utility.dart';
 import 'package:iconsax/iconsax.dart';
-
-import '../../../../common/widgets/products.cart/cart_menu_icon.dart';
 import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
 import '../widgets/home_appbar.dart';
-import '/common/widgets/appbar/appbar.dart';
-import '/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
@@ -15,13 +13,37 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-            child: Column(
-      children: [
-        TPrimaryHeaderContainer(
-            child: Column(
-          children: [THomeAppBar()],
-        )),
-      ],
-    )));
+            child: Column(children: [
+      TPrimaryHeaderContainer(
+          child: Column(children: [
+        THomeAppBar(),
+        const SizedBox(height: TSizes.spaceBtwSections),
+
+        /// SearchBar
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+          child: Container(
+            width: TDeviceUtils.getScreenWidth(context),
+            padding: const EdgeInsets.all(TSizes.md),
+            decoration: BoxDecoration(
+              color: TColors.white,
+              borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+              border: Border.all(color: TColors.grey),
+            ),
+            child: Row(
+              children: [
+                const Icon(
+                  Iconsax.search_normal,
+                  color: TColors.darkerGrey,
+                ),
+                const SizedBox(width: TSizes.spaceBtwItems),
+                Text('Rechercher dans les restaurants',
+                    style: Theme.of(context).textTheme.bodySmall!.apply(color: TColors.darkerGrey)),
+              ],
+            ),
+          ),
+        ),
+      ]))
+    ])));
   }
 }
